ef0d9a31d8a7bcb9e499638f30c327baab66b014
Edward <edward.heaver01@gmail.com>
2021-04-23 19:39:20 +0100

    refactored posts to be a grid

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -7,7 +7,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PostService {
-  postsURL: string = "https://jsonplaceholder.typicode.com/posts"
+  // Fallback url as jsonplaceholder decided it didn't want to work once
+  // https://mockend.com/Captainmango/xplore-app/posts
+
+  // JSONPlaceholder url - looks better and isn't complete gibberish
+  // https://jsonplaceholder.typicode.com/posts
+  
+  postsURL: string = "https://mockend.com/Captainmango/xplore-app/posts"
 
   constructor(private http: HttpClient) { }
 
