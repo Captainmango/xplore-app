94573373cad31ac4b09a3b0ef4f6109cdfc21236
Edward <edward.heaver01@gmail.com>
2021-05-04 21:25:55 +0100

    refactored post-show to use async pipe

--- src/app/components/post-show/post-show.component.ts
+++ src/app/components/post-show/post-show.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Post } from 'src/app/models/Post';
 import { PostService } from 'src/app/services/post.service';
 
@@ -12,18 +14,15 @@ export class PostShowComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private postService: PostService) { }
 
-  @Input() post!: Post
+  @Input() post$!: Observable<Post>
+  
 
   postID = this.route.snapshot.params.id
 
   // TODO: work out how to make this async. The mark up updates as expected, but the console shows undefined errors.
 
   ngOnInit(): void {
-    this.postService.getPost(this.postID).subscribe(post => {
-      this.post = post
-    })
-
-    console.log(this.post)
+    this.post$ = this.postService.getPost(this.postID)
   }
 
 }
